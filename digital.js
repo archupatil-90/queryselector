481f06aa12c4a38579ed1b80ceb42ec5149a55f7
archupatil-90 <73097336+archupatil-90@users.noreply.github.com>
2021-01-05 11:44:33 +0530

    Add files via upload

---
+++ digital.js
@@ -0,0 +1,22 @@
+let data=document. getElementsByTagName('img');
+console.log(data);
+let data1=document.links;
+console.log(data1);
+// let data3=document.getElementsByClassName("img").alt;
+// console.log(data3);
+
+let d1=document.querySelector(".img").alt.length;
+console.log(d1);
+
+let d2=document.querySelector(".cardbox-container").classList.length;
+console.log(d2);
+let d3=document.querySelector("i").classList.item(1);
+console.log(d3);
+let link1=document.querySelector(".cardbox").classList.add("abc","xyz");
+console.log(link1);
+let link2=document.querySelector(".cardbox").classList.remove("abc");
+console.log(link2);
+let d4=document.querySelector(".elementor-button").style.backgroundColor="red";
+console.log(d4);
+let d5=document.querySelector("i").className;
+console.log(d5);
